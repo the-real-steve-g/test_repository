b38530884e2c32268cdcd34c92abb37e08b18b70
Stephen Goh <stephen.goh1@gmail.com>
2020-02-09 22:44:04 +1100

    added a new list sum function

--- test.py
+++ test.py
@@ -1,9 +1,19 @@
+'''Docstring here'''
 
-'''
-Docstring here
-'''
-### This is a helloworld program ###
+# This is a helloworld program ###
+# ctrl shift b is used to run
+print("helloworld")
+print("Save this file")
+print("Maybe one more line to test")
 
-print ("helloworld")
-print ("Save this file")
-print ("Maybe one more line to test")
+
+def list_sum(list):
+    """This sums up a list"""
+    return sum(list)
+
+
+k = [1, 4, 7]
+print(list_sum(k))
+
+print("results")
+help(list_sum)
